1dbc866fba69e76cd6e1d1e0ad410f8a2f04fa47
Matiko <mmatiko243@gmail.com>
2022-05-21 20:42:46 +0000

    Updated main.h

--- main.h
+++ main.h
@@ -20,19 +20,18 @@ typedef struct code_format
 int _putchar(char c);
 int _puts(char *str);
 int _printf(const char *format, ...);
-int print_char(va_list);
-int print_string(va_list);
-int print_dec(va_list);
-int print_hex(va_list x);
-int print_heX(va_list X);
-int print_octal(va_list o);
-int print_binary(va_list);
-int print_unsigned(va_list);
-int print_rot13(va_list);
-int print_reversed(va_list r);
+int print_char(va_list c);
+int print_string(va_list s);
+int print_decimal(va_list d);
+int print_hexa(va_list x);
+int print_HEXA(va_list X);
+int print_oct(va_list o);
+int print_binary(va_list b);
+int print_unsigned(va_list u);
+int print_rot13(va_list R);
+int print_reverse(va_list r);
 int print_int(va_list i);
 int print_S(va_list S);
 int print_p(va_list p);
-
 
 #endif
