0fbf009359328c272c72ec5ea29502ab6e09ce6d
AthmanDodoe <jilloathman943@gmail.com>
2022-05-22 08:36:42 +0300

    modified main file

--- main.h
+++ main.h
@@ -7,6 +7,8 @@
 #include <stdio.h>
 #include <limits.h>
 
+#define NULL_STRING "(null)"
+#define NUL '\0'
 
 /**
  * struct code_format - structure format
@@ -14,12 +16,14 @@
  * @f: function associated
  */
 
-typedef struct code_format
+struct convert
 {
   char *sc;
   int (*f)(va_list);
-} code_f;
+};
+typedef struct convert conver_t;
 
+int format_reciever(const char *format, conver_t f_list[], va_list arg_list);
 int _putchar(char c);
 int _puts(char *str);
 int _printf(const char *format, ...);
@@ -38,4 +42,18 @@ int print_int(va_list i);
 int print_S(va_list S);
 int print_p(va_list p);
 
+int print_number(va_list args);
+unsigned int base_len(unsigned int, int);
+char *rev_string(char *);
+void write_base(char *str);
+char *_memcpy(char *dest, char *src, unsigned int n);
+int print_unsgined_number(unsigned int n);
+int hex_check(int num, char x);
+int print_hex_aux(unsigned long int num);
+int isNonAlphaNumeric(char c); 
+int _puts(char *str);
+char *convert(unsigned long int num, int base, int lowercase);
+
 #endif
+
+
